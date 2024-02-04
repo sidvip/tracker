3120ebdea9d9013745dd6810716ebd330d4fda72
Siddhartha Vibhu Pharswan <{ID}+{username}@users.noreply.github.com>
2024-02-05 03:31:16 +0530

    - google login

--- index.js
+++ index.js
@@ -7,14 +7,39 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
             console.log('logged in backend')
         }
     };
-    xhttp.open("GET", `http://localhost:8000/add-url?email=${'abc@gmail.com'}&url=${message.greeting}`, true);
-    xhttp.send();
+    chrome.storage.local.get("token", function (items) {
+        console.log(items);
+        if (items?.token) {
+            // document.getElementById("signed-in").style.display = "block";
+            xhttp.open("GET", `https://tracker-server-w47v.onrender.com/add-url?email=${items?.token}&url=${message.greeting}`, true);
+            xhttp.send();
+        }
+    });
 });
 
 window.onload = function () {
-    document.querySelector('#sign-in').addEventListener('click', function () {
+
+    document.querySelector('#sign-in')?.addEventListener('click', function () {
         chrome.identity.getAuthToken({ interactive: true }, function (token) {
-            console.log(token);
+            chrome.storage.local.set({ "token": token }, function () {
+                console.log('set');
+            });
         });
     });
+
+    document.querySelector('#sign-out')?.addEventListener('click', function () {
+        chrome.identity.launchWebAuthFlow(
+            { 'url': 'https://accounts.google.com/logout' },
+            function (tokenUrl) {
+                chrome.storage.local.clear(function () {
+                    console.log('cleaned storage');
+                    document.getElementById("signed-in").style.display = "none";
+                });
+                console.log(tokenUrl);
+                // responseCallback();
+            }
+        );
+    });
+
+
 };
